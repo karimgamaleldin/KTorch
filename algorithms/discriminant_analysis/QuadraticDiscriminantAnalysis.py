533fddecbf843e4d1ef5de000dca7eb24469455e
Karim Gamaleldin <karimgamaleldin74@gmail.com>
2024-04-25 13:58:59 +0200

    feat: add LDA algorithm and example notebook

--- algorithms/discriminant_analysis/QuadraticDiscriminantAnalysis.py
+++ algorithms/discriminant_analysis/QuadraticDiscriminantAnalysis.py
@@ -2,5 +2,4 @@ from core.BaseEstimator import BaseEstimator
 import numpy as np
 
 class QuadraticDiscriminantAnalysis(BaseEstimator):
-  def __init__(self, algorithm_name, algorithm_type, algorithm_params):
-    super().__init__(algorithm_name, algorithm_type, algorithm_params)
+  pass 
